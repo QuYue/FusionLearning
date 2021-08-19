cbe32755f7be8f59f70b5e28541b55da005ed6c2
QuYue <quyue1541@gmail.com>
2021-08-19 16:43:07 +0800

    1

--- Experiments/Experiment1/data_input.py
+++ Experiments/Experiment1/data_input.py
@@ -7,5 +7,30 @@
 @Email   :   quyue1541@gmail.com
 @Desc    :   Input data.
 """
+#%% Import Packages
 import torch
-print(1)
+import torchvision
+from sklearn.model_selection import train_test_split
+
+#%% Fusions
+def data_input(dataset_no=1, download=True):
+    # Input Data
+    if dataset_no == 1 or 'Disjoint_MNIST':
+        dataset_no = 1
+        print('Loading dataset 1： Disjoint_MNIST...')
+        data_train, data_test = MNIST_input(download)
+
+
+def MNIST_input(download=True):
+    # Data1 MNIST
+    trans = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0.5,), (1.0,))])
+    data_train = torchvision.datasets.MNIST(root="../../Data/MNIST",
+                                            train=True,
+                                            transform=trans,
+                                            download=download)
+    data_test = torchvision.datasets.MNIST(root="../../Data/MNIST",
+                                           train=False,
+                                           transform=trans,
+                                           download=download)
+    return data_train, data_test
